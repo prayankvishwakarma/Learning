51b5d9ecd9f27a4feca1e3afd2fdf36301439ce3
Prayank Vishwakarma <prayankv3@gmail.com>
2023-04-03 03:42:40 +0530

    CourseDetails

---
+++ screens/Course/CourseTabs/CourseChapters.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, ScrollView, Text, Image, FlatList} from 'react-native';
+import {
+  IconLabel,
+  TextButton,
+  HorizontalCourseCard,
+  LineDivider,
+} from '../../../components';
+
+import {
+  COLORS,
+  FONTS,
+  SIZES,
+  images,
+  icons,
+  dummyData,
+} from '../../../constants';
+
+const CourseChapters = () => {
+  return (
+    <View>
+      <Text>CourseChapters</Text>
+    </View>
+  );
+};
+
+export default CourseChapters;
